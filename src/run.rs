91e49d4bc2f16ee1734561508f933ca5d22036f2
goldstraw <goldstrawcharlie@gmail.com>
2023-06-14 09:26:42 +0100

    Change print statements to logging

--- src/run.rs
+++ src/run.rs
@@ -4,6 +4,7 @@ use rand::Rng;
 use crate::embedding::load_embeddings;
 use crate::transformer::Transformer;
 use crate::dataset::load_imdb_dataset;
+use log::info;
 
 pub fn run() {
     let word_embeddings = load_embeddings("word_embeddings.json");
@@ -11,7 +12,8 @@ pub fn run() {
     let mut transformer = Transformer::new(40, 200, 3, 3, arr1(&[8000,1000,8000]), word_embeddings);
     let mut rng = rand::thread_rng();
 
-    let mut prev_100 = arr1(&[0.0; 100]);
+    const N: usize = 100;
+    let mut prev_100 = arr1(&[0.0; N]);
     let mut index = 0;
 
     loop {
@@ -19,9 +21,9 @@ pub fn run() {
         let val = transformer.forward_propagate(example.review.clone());
         prev_100[index] = (val - example.sentiment).powf(2.0);
         index += 1;
-        if index == 100 {
+        if index == N {
             index = 0;
-            println!("{:?}", prev_100.sum() / 100.0);
+            info!("{:?}", prev_100.sum() / N as f32);
         }
         transformer.back_propagate(example.sentiment);
     }
