f3a5c4256b09dab42e1a3e5f60c93769eea55270
goldstraw <goldstrawcharlie@gmail.com>
2023-06-16 08:39:15 +0100

    Add testing dataset functionality

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use rusttransformer::*;
-use log::{LevelFilter};
+use log::LevelFilter;
+use std::io;
 
 fn main() {
     // Set the custom logger as the global logger
@@ -10,5 +11,30 @@ fn main() {
         log::set_max_level(LevelFilter::Error);
     }
 
-    run::run();
+    println!("Enter the max number of words: ");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read input.");
+    let num_words = input.trim().parse().expect("Invalid input.");
+
+    println!("Enter the dimensionality: ");
+    input.clear();
+    io::stdin().read_line(&mut input).expect("Failed to read input.");
+    let dimensionality = input.trim().parse().expect("Invalid input.");
+
+    println!("Enter the number of encoders: ");
+    input.clear();
+    io::stdin().read_line(&mut input).expect("Failed to read input.");
+    let num_encoders = input.trim().parse().expect("Invalid input.");
+
+    println!("Enter the number of heads: ");
+    input.clear();
+    io::stdin().read_line(&mut input).expect("Failed to read input.");
+    let num_heads = input.trim().parse().expect("Invalid input.");
+
+    println!("Enter the hidden layer size: ");
+    input.clear();
+    io::stdin().read_line(&mut input).expect("Failed to read input.");
+    let hidden_layer_size = input.trim().parse().expect("Invalid input.");
+
+    run::run(num_words, dimensionality, num_encoders, num_heads, hidden_layer_size);
 }
--- src/run.rs
+++ src/run.rs
@@ -5,56 +5,34 @@ use crate::embedding::load_embeddings;
 use crate::transformer::Transformer;
 use crate::dataset::load_imdb_dataset;
 use log::info;
-use std::io;
 use std::fs::OpenOptions;
 use std::io::{BufWriter, Write};
 use crate::LR;
 use std::time::SystemTime;
 
-pub fn run() {
+pub fn run(num_words: usize, dimensionality: usize, num_encoders: usize, num_heads: usize, hidden_layer_size: usize) {
     let prog_start = SystemTime::now();
-    println!("Enter the max number of words: ");
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Failed to read input.");
-    let num_words = input.trim().parse().expect("Invalid input.");
-
-    println!("Enter the dimensionality: ");
-    input.clear();
-    io::stdin().read_line(&mut input).expect("Failed to read input.");
-    let dimensionality = input.trim().parse().expect("Invalid input.");
-
-    println!("Enter the number of encoders: ");
-    input.clear();
-    io::stdin().read_line(&mut input).expect("Failed to read input.");
-    let num_encoders = input.trim().parse().expect("Invalid input.");
-
-    println!("Enter the number of heads: ");
-    input.clear();
-    io::stdin().read_line(&mut input).expect("Failed to read input.");
-    let num_heads = input.trim().parse().expect("Invalid input.");
-
-    println!("Enter the hidden layer size: ");
-    input.clear();
-    io::stdin().read_line(&mut input).expect("Failed to read input.");
-    let hidden_layer_size = input.trim().parse().expect("Invalid input.");
-
 
     let word_embeddings = load_embeddings("word_embeddings.json");
     let dataset = load_imdb_dataset("imdb_dataset.csv", num_words, word_embeddings.clone());
     let mut transformer = Transformer::new(num_words, dimensionality, num_encoders, num_heads, arr1(&[num_words*dimensionality,hidden_layer_size,num_words*dimensionality]), word_embeddings);
     let mut rng = rand::thread_rng();
 
-    const N: usize = 10;
+    const N: usize = 1000;
     let mut prev_100 = arr1(&[0.0; N]);
     let mut index = 0;
+    let test_gaps = 5;
+    let mut test_count = 0;
+    const TEST_SIZE: usize = 200;
 
     loop {
-        let example = &dataset[rng.gen_range(0..dataset.len())];
+        let example = &dataset[rng.gen_range(0..dataset.len()-TEST_SIZE)];
         let val = transformer.forward_propagate(example.review.clone());
         prev_100[index] = (val - example.sentiment).powf(2.0);
         index += 1;
         if index == N {
             index = 0;
+            test_count += 1;
             info!("{:?}", prev_100.sum() / N as f32);
 
             let file_path = format!("MW{}_D{}_E{}_H{}_Hi{}_LR{}", num_words, dimensionality, num_encoders, num_heads, hidden_layer_size, LR);
@@ -73,6 +51,34 @@ pub fn run() {
 
             let mut file_writer = BufWriter::new(file);
             writeln!(file_writer, "{}", line_to_add).expect("Failed to write to file");
+
+            if test_count == test_gaps {
+                test_count = 0;
+                let mut test_cost = arr1(&[0.0; TEST_SIZE]);
+                for i in 0..TEST_SIZE {
+                    let example = &dataset[dataset.len()-TEST_SIZE+i];
+                    let val = transformer.forward_propagate(example.review.clone());
+                    test_cost[i] = (val - example.sentiment).powf(2.0);
+                }
+                info!("TEST - {:?}", test_cost.sum() / TEST_SIZE as f32);
+
+                let file_path = format!("TMW{}_D{}_E{}_H{}_Hi{}_LR{}", num_words, dimensionality, num_encoders, num_heads, hidden_layer_size, LR);
+                let current_time = SystemTime::now()
+                    .duration_since(prog_start)
+                    .expect("Failed to get current time")
+                    .as_secs();
+                let line_to_add = format!("{},{}", current_time, test_cost.sum() / TEST_SIZE as f32);
+
+                // Open the file in append mode or create it if it doesn't exist
+                let file = OpenOptions::new()
+                    .create(true)
+                    .append(true)
+                    .open(file_path)
+                    .expect("Failed to open file");
+
+                let mut file_writer = BufWriter::new(file);
+                writeln!(file_writer, "{}", line_to_add).expect("Failed to write to file");
+            }
         }
         transformer.back_propagate(example.sentiment);
     }
